ea533ccbff485969b9139325c3c53a4b3a486969
TheNeonHaze <TheNeonHaze@github.com>
2020-04-16 17:19:27 +0600

    Beta V.0.1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print('heloo')
-print('dsds')
-##
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Привет какаха"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
