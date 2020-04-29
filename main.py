2925981d26bda121eccef8aaf75a28204f3c4ae6
Daniil Shatokhin <57665228+Dalbick@users.noreply.github.com>
2020-04-29 16:44:35 +0600

    Beta V.0.2

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, abort
 import schedule as sh
 import mnepofig
-
 
 
 app = Flask(__name__)
@@ -11,14 +9,17 @@ mnepofig.get_time_schedule()
 mnepofig.get_all_schedule()
 
 
-@app.route('/')
-@app.route('/schedule')
-
-def schedule():
-    return render_template('index.html', title='YARIK LOX', schedule=mnepofig.read_json("7D"), status=30)
+@app.route('/schedule/<grade>')
+def schedule(grade):
+    if grade in mnepofig.read_json('clases'):
+        return render_template('schedule.html', title=grade + ' schedule',
+                               schedule=mnepofig.read_json(grade), grade=grade,
+                               time=mnepofig.read_json('time'))
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
-    #sh.every(5).minutes.do(mnepofig.get_all_schedule)
-    #sh.run_pending()
-    app.run(port=8080, host='127.0.0.1')
+    # sh.every(5).minutes.do(mnepofig.get_all_schedule)
+    # sh.run_pending()
+    app.run()
--- mnepofig.py
+++ mnepofig.py
@@ -4,20 +4,18 @@ import gspread
 import re
 import json
 import time
+
+
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-
 creds = ServiceAccountCredentials.from_json_keyfile_name('secret.json', scope)
-
 client = gspread.authorize(creds)
-
 weekday = datetime.date.today().weekday() + 1
-
 subjects_col = range(5, 15)
-
 wks = client.open("Fizmat Online 2019-20")
 
-def get_day_schedule(grade): #Получает расписание на день
+
+def get_day_schedule(grade):  # Получает расписание на день
     lsch = wks.worksheet(grade)
     subject = []
     for i in subjects_col:
@@ -25,19 +23,37 @@ def get_day_schedule(grade): #Получает расписание на ден�
     write_json(subject, grade)
     return subject
 
-def get_all_titles(): #Получает литеру классов
+
+def get_all_titles():  # Получает литеру классов
     titles_list = str(wks.worksheets())
     clear_titles = re.findall(r"[']+\w+", titles_list)
     clear_titles = list(map(lambda x: x[1:], clear_titles))
     write_json(clear_titles, "clases")
 
-def get_all_schedule(): #Получает все расписание
+
+def get_all_schedule():  # Получает все расписание
     grades = read_json("clases")
     for i in grades:
         get_week_schedule(i)
         time.sleep(10)
+    for grade in read_json('clases'):
+        modified_grade = list()
+        for day in read_json(grade):
+            modified_day = list()
+            for lesson in day:
+                modified_lesson = lesson
+                if lesson:
+                    words = lesson.split()
+                    if (words[-1].isdigit() or words[-1] == 'спорт.зал' or
+                            (words[-1].split('/')[0].isdigit() and
+                             words[-1].split('/')[1].isdigit())):
+                        modified_lesson = ' '.join(words[:-1]) + '\n' + words[-1]
+                modified_day.append(modified_lesson)
+            modified_grade.append(modified_day)
+        write_json(modified_grade, grade)
 
-def get_time_schedule(): #Получает время
+
+def get_time_schedule():  # Получает время
     lsch = wks.sheet1
     subject = []
     for i in subjects_col:
@@ -45,7 +61,8 @@ def get_time_schedule(): #Получает время
     write_json(subject, "time")
     return subject
 
-def get_week_schedule(grade): #Получает расписание на неделю
+
+def get_week_schedule(grade):  # Получает расписание на неделю
     lsch = wks.worksheet(grade)
     k = lsch.get_all_values()
     k = list(map(lambda x: x[3:], k))
@@ -56,10 +73,11 @@ def get_week_schedule(grade): #Получает расписание на нед
 
 
 def write_json(data, grade):
-    with open('schedule_cache/schedule_'+grade+'.json', 'w') as outfile:
+    with open('schedule_cache/schedule_'+grade+'.json', 'w', encoding='utf-8') as outfile:
         json.dump(data, outfile, indent=2, ensure_ascii=False)
 
+
 def read_json(grade):
-    with open('schedule_cache/schedule_'+grade+'.json') as outfile:
+    with open('schedule_cache/schedule_'+grade+'.json', encoding='utf-8') as outfile:
         data = json.load(outfile)
         return data
